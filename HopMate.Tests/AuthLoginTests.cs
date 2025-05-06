3094c2490e3dd1f392f928d63a56645f766dfae3
bntia <bntia@DESKTOP-4PCFCD1>
2025-05-06 10:42:02 +0100

    Adicionar arquivos de projeto.

---
+++ HopMate.Tests/AuthLoginTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hopmate.Server; // Altere se necessário para o namespace correto
+
+namespace hopmate.Server.Tests
+{
+    [TestClass]
+    public class AuthControllerTests
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthControllerTests()
+        {
+            var appFactory = new WebApplicationFactory<Program>();
+            _httpClient = appFactory.CreateDefaultClient();
+        }
+
+        [TestMethod]
+        public async Task Login_Valido_DeveRetornarOk()
+        {
+            var loginData = new
+            {
+                Username = "zuca06",
+                Password = "Tiagofer1402."
+            };
+
+            var response = await _httpClient.PostAsJsonAsync("api/Auth/login", loginData);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task Login_Invalido_DeveRetornarUnauthorized()
+        {
+            var loginData = new
+            {
+                Username = "zuca06",
+                Password = "senhaIncorreta123"
+            };
+
+            var response = await _httpClient.PostAsJsonAsync("api/Auth/login", loginData);
+
+            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task Login_ComCamposVazios_DeveRetornarBadRequest()
+        {
+            var loginData = new
+            {
+                Username = "",
+                Password = ""
+            };
+
+            var response = await _httpClient.PostAsJsonAsync("api/Auth/login", loginData);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
---
+++ hopmate.Server/Migrations/20250506074424_hopmate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hopmate.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class hopmate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
